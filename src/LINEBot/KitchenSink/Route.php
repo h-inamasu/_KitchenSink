eee828308203410e60801232300210b5d7e6d518
Hideyuki Inamasu <h-inamasu@1onepiece.jp>
2018-09-25 19:00:34 -0700

    x

--- src/LINEBot/KitchenSink/Route.php
+++ src/LINEBot/KitchenSink/Route.php
@@ -119,7 +119,9 @@ class Route
                     continue;
                 }
 
+error_log("Before $handler->handle()");
                 $handler->handle();
+error_log("After $handler->handle()");
             }
 
             $res->write('OK');
