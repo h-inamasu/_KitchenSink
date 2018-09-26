75b562eda813a8e6535af4a62a261508cd2dd61e
Hideyuki Inamasu <h-inamasu@1onepiece.jp>
2018-09-26 15:32:37 -0700

    Debug info

--- src/LINEBot/KitchenSink/EventHandler/FollowEventHandler.php
+++ src/LINEBot/KitchenSink/EventHandler/FollowEventHandler.php
@@ -46,6 +46,7 @@ class FollowEventHandler implements EventHandler
 
     public function handle()
     {
+        $this->bot->replyText($this->followEvent->getReplyToken(),"お友達登録\nありがとうございます");
         $this->bot->replyText($this->followEvent->getReplyToken(),'お友達登録\r\nありがとうございます');
         $this->bot->replyText($this->followEvent->getReplyToken(),'いつでもお気軽にお問い合わせメッセージをお送りください！');
         $this->bot->replyText($this->followEvent->getReplyToken(),'シューワのお水をご利用中のお客様は');
